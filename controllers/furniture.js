205c12505be52775c891f089cda35b358925f8ac
Jaswitha-20 <s562626@nwmissouri.edu>
2023-11-10 11:40:49 -0600

    added new data

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -61,6 +61,39 @@ exports.furniture_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+// for a specific Costume.
+exports.furniture_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await furniture.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+    // Handle Costume update form on PUT.
+exports.furniture_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await furniture.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Furniture) toUpdate.Furniture = req.body.Furniture;
+    if(req.body.Design) toUpdate.Design = req.body.Design;
+    if(req.body.Cost) toUpdate.Cost = req.body.Cost;
+    if(req.body.Brand) toUpdate.Brand = req.body.Brand;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+    
 
 
 
--- routes/furniture.js
+++ routes/furniture.js
@@ -4,3 +4,6 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', furniture_controlers.furniture_view_all_Page );
 module.exports = router;
+
+// GET request for one costume.
+router.get('/furniture/:id', furniture_controlers.furniture_detail)
