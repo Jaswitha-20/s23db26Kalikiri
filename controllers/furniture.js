b141a836c9ad7868a67580e398fcb243987cb045
Jaswitha-20 <s562626@nwmissouri.edu>
2023-11-14 19:10:13 -0600

    updated new data

--- controllers/furniture.js
+++ controllers/furniture.js
@@ -136,6 +136,20 @@ res.send(`{'error': '${err}'}`);
 }
 };
 
+// Handle building the view for updating a costume.
+// query provides the id
+exports.furniture_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await furniture.findById(req.query.id)
+res.render('furnitureupdate', { title: 'Furniture Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
 
 
 
--- routes/furniture.js
+++ routes/furniture.js
@@ -13,3 +13,6 @@ router.get('/detail', furniture_controlers.furniture_view_one_Page);
 
 /* GET create costume page */
 router.get('/create', furniture_controlers.furniture_create_Page);
+
+/* GET create update page */
+router.get('/update', furniture_controlers.furniture_update_Page);
