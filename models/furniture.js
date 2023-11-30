16c73f1a4a87efcfe3e934d896d2e52d7215b702
Jaswitha-20 <s562626@nwmissouri.edu>
2023-11-29 23:19:03 -0600

    added validators

--- models/furniture.js
+++ models/furniture.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose")
 const furnitureSchema = mongoose.Schema({
-Furniture: String,
-Design: String,
-Cost: Number,
-Brand: String
+    Furniture: {
+        type: String,
+        required: true
+    },
+    Design: String,
+    Cost: {
+        type: Number,
+        min: 2000,
+        max: 2000000
+    },
+    Brand: String
 })
-module.exports = mongoose.model("furniture",furnitureSchema);
+module.exports = mongoose.model("furniture", furnitureSchema);
--- routes/furniture.js
+++ routes/furniture.js
@@ -11,8 +11,6 @@ router.get('/furniture/:id', furniture_controlers.furniture_detail)
 /* GET detail costume page */
 router.get('/detail', furniture_controlers.furniture_view_one_Page);
 
-/* GET create costume page */
-router.get('/create', furniture_controlers.furniture_create_Page);
 
 /* GET create update page */
 // A little function to check if we have an authorized user and continue on or
@@ -26,4 +24,6 @@ res.redirect("/login");
 router.get('/update',secured, furniture_controlers.furniture_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', furniture_controlers.furniture_delete_Page);
+router.get('/delete',secured, furniture_controlers.furniture_delete_Page);
+
+router.get('/create',secured, furniture_controlers.furniture_create_Page);
